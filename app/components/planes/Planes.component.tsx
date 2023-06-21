d972657602b182d6c3ef4d21322738d852fadeb1
Rmichal503 <rakmichal503@gmail.com>
2023-06-21 22:08:19 +0200

    first try vercel dep

--- app/components/planes/Planes.component.tsx
+++ app/components/planes/Planes.component.tsx
@@ -1,9 +1,59 @@
-import Image from "next/image";
+import { useState } from 'react'
+import Airbus from '@/public/planes/airbus.svg'
+import Boeing from '@/public/planes/boeing.svg'
+import Emb from '@/public/planes/emb.svg'
 
-export default function Planes(){
+interface PlanesProps {
+    typeOfPlane : string
+}
 
+export default function Planes({typeOfPlane}:PlanesProps){
+const [seats, setSeats] = useState<Array<String[]>>([])
+    let plane
+    switch (typeOfPlane) {
+        case 'emb':
+            plane = <Emb 
+            onClick={(e:Event)=>{
+                const parent = (e.target as HTMLElement)?.parentNode;
+                const targetClass = parent instanceof HTMLElement ? parent.classList[0] : null;
+                // const targetClass = (e.target as HTMLElement)?.parentNode?.classList[0];
+                if(targetClass === null) return
+                const seatTypeAndNumb = targetClass.replace('emb_svg__','').split('_')
+                console.log(seatTypeAndNumb);
+                setSeats((prev)=>{
+                    return [...prev,seatTypeAndNumb]
+                })
+                console.log(seats);
+                }} />
+            break;
+        case 'boeing':
+            plane = <Boeing 
+            onClick={(e:Event)=>{
+                const parent = (e.target as HTMLElement)?.parentNode;
+                const targetClass = parent instanceof HTMLElement ? parent.classList[0] : null;
+                // const targetClass = (e.target as HTMLElement).parentNode?.classList[0];
+                if(targetClass === null) return
+                const seatTypeAndNumb = targetClass.replace('boeing_svg__','').split('_')
+                console.log(seatTypeAndNumb);
+                }} />
+            break;
+        case 'airbus':
+            plane = <Airbus 
+            onClick={(e:Event)=>{
+                const parent = (e.target as HTMLElement)?.parentNode;
+                const targetClass = parent instanceof HTMLElement ? parent.classList[0] : null;
+                // const targetClass = (e.target as HTMLElement).parentNode?.classList[0];
+                if(targetClass === null) return
+                const seatTypeAndNumb = targetClass.replace('airbus_svg__','').split('_')
+                console.log(seatTypeAndNumb);
+                }} />
+            break;
+        default:
+            break;
+    }
     return (
-        // <Image src={'/emb.svg'} alt='emb' height={500} width={200} />
-        <img src="'/airbus.svg'" alt="emb" />
+        <>
+        {plane === undefined ? null : plane}
+        </>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 'use client'
-import axios from 'axios'
-import dayjs from 'dayjs'
 import { useEffect, useState } from 'react'
 import FlightSearch from './components/flightSearch/FlightSearch.component'
 import Weather, { WeatherProps } from './components/weather/Weather.component'
-import Airbus from '../public/airbus.svg'
-import Boeing from '../public/boeing.svg'
-import Emb from '../public/emb.svg'
+import Planes from './components/planes/Planes.component'
 interface WeatherState{
   apparent_temperature:string,
   precipitation_probability:string
@@ -40,37 +36,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-500">
       {/* {Object.keys(weather).length !=0?<Weather weatherProps={weather}/>:null} */}
       <FlightSearch/>
-      <Emb
-      onClick={(e:Event)=>{
-        const targetClass = (e.target as HTMLElement).parentNode.classList[0];
-        if(targetClass === undefined) return
-        const seatTypeAndNumb = targetClass.replace('emb_svg__','').split('_')
-        console.log(seatTypeAndNumb);
-        }}
-        />
-      <Boeing
-      onClick={(e:Event)=>{
-        console.log(e);
-        const targetClass = (e.target as HTMLElement).parentNode.classList[0];
-        const seatCheck = e.target.parentElement.attributes[0].value
-        if(seatCheck === '#43A9C7'){
-          e.target.parentElement.attributes[0].value = '#000000'
-        } else {
-          e.target.parentElement.attributes[0].value = '#43A9C7'
-        }
-        if(targetClass === undefined) return
-        const seatTypeAndNumb = targetClass.replace('boeing_svg__','').split('_')
-        console.log(seatTypeAndNumb);
-        }}
-        />
-      <Airbus
-      onClick={(e:Event)=>{
-        const targetClass = (e.target as HTMLElement).parentNode.classList[0];
-        if(targetClass === undefined) return
-        const seatTypeAndNumb = targetClass.replace('airbus_svg__','').split('_')
-        console.log(seatTypeAndNumb);
-        }}
-        />
+      <Planes typeOfPlane={'emb'}/>
+
     </main>
   )
 }
